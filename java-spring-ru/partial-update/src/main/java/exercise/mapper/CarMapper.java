29de0dad1b90ab43f60b417570e5aed76aec51f7
@hexlet/cli <support@hexlet.io>
2024-08-27 01:48:23 +0300

    submit java-spring-ru/partial-update

---
+++ java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
@@ -0,0 +1,27 @@
+package exercise.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import exercise.dto.CarCreateDTO;
+import exercise.dto.CarUpdateDTO;
+import exercise.dto.CarDTO;
+import exercise.model.Car;
+
+// BEGIN
+@Mapper(
+        uses = {JsonNullableMapper.class},
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public abstract class CarMapper {
+    public abstract Car map (CarCreateDTO createDTO);
+    public abstract CarDTO map (Car car);
+    public abstract void update (CarUpdateDTO updateDTO, @MappingTarget Car car);
+
+}
+// END
